256ae1abd181ae3af42cfebc79aae8e13a65cae2
Luiz Paulo <luizpaulosz@hotmail.com>
2018-10-24 08:12:51 -0300

    pwa 2

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+let cacheName = 'cas-1.0.0';
+let filesToCache = [
+    './',
+    'index.html',
+    'css/framework7.min.css',
+    'js/framework7.min.js'
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Installer');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
